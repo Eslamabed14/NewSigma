8943d3ddd2229ce926f8768941c15b9db45ec8a1
Eslamabed14 <eslamabed1911@gmail.com>
2023-05-21 06:19:56 +0300

    Create main.js

---
+++ public/home/js/main.js
@@ -0,0 +1,121 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($("#spinner").length > 0) {
+                $("#spinner").removeClass("show");
+            }
+        }, 1);
+    };
+    spinner();
+
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $(".navbar").addClass("sticky-top shadow-sm");
+        } else {
+            $(".navbar").removeClass("sticky-top shadow-sm");
+        }
+    });
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".back-to-top").fadeIn("slow");
+        } else {
+            $(".back-to-top").fadeOut("slow");
+        }
+    });
+    $(".back-to-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 0, "easeInOutExpo");
+        return false;
+    });
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>',
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            },
+        },
+    });
+
+    // Client carousel
+    $(".client-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 90,
+        dots: false,
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            576: {
+                items: 3,
+            },
+            768: {
+                items: 4,
+            },
+            992: {
+                items: 5,
+            },
+            1200: {
+                items: 6,
+            },
+        },
+    });
+})(jQuery);
+
+function showDis(partner) {
+    swal({
+        position: "top-end",
+        icon: "success",
+        title: " قيمة الخصم تصل الى: " + `${partner}` + " %",
+        showConfirmButton: false,
+        buttons: {
+            confirm: {
+                className: "sweet-warning",
+            },
+        },
+    });
+}
+
+function success() {
+    swal({
+        position: "top-end",
+        icon: "success",
+        title: "تم تسجيل بياناتك وجاري مراجعتها من الإدارة .. سوف يتم ارسال ايميل لك عند الموافقة",
+        showConfirmButton: false,
+        buttons: {
+            confirm: {
+                className: "sweet-warning",
+            },
+        },
+    });
+}
